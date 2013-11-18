0be824d7c68a034b65c20b6e3c23d762fddc564a
dpgaspar <danielvazgaspar@gmail.com>
2013-11-18 16:07:39 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask.ext.appbuilder.baseapp import BaseApp
+from flask.ext.appbuilder.models.datamodel import SQLAModel
 from flask.ext.appbuilder.views import GeneralView
 from app import app, db
 
