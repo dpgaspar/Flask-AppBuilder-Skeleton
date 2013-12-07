6aca6296fb35c47f52b5e19778ae4b31d7649cb6
dpgaspar <danielvazgaspar@gmail.com>
2013-12-07 23:08:35 +0000

    version 0.3.X update

--- babel_extract.sh
+++ babel_extract.sh
@@ -1,5 +1,5 @@
-pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
-pybabel update -i ./babel/messages.pot -d app/translations
-pybabel compile -d app/translations
+#pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
+pybabel update -N -i ./babel/messages.pot -d app/translations
+pybabel compile -f -d app/translations
 
 
