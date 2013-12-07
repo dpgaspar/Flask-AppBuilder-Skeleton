25d58ec98e4e640d1d48d6d27300c242e43ef6a0
dpgaspar <danielvazgaspar@gmail.com>
2013-12-07 20:55:22 +0000

    babel support

--- config.py
+++ config.py
@@ -19,7 +19,8 @@ BABEL_DEFAULT_LOCALE = 'en'
 
 LANGUAGES = {
     'en': {'flag':'gb', 'name':'English'},
-    'pt': {'flag':'pt', 'name':'Portugal'}
+    'pt': {'flag':'pt', 'name':'Portugal'},
+    'es': {'flag':'es', 'name':'Spain'}
 }
 
 
