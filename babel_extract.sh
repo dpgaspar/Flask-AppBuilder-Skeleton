b9f86a37ee4239a1950d91bccfcad6c300eb0a62
dpgaspar <danielvazgaspar@gmail.com>
2014-01-13 14:43:43 +0000

    babel_init correction

--- babel_extract.sh
+++ babel_extract.sh
@@ -1,4 +1,4 @@
-#pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
+pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
 pybabel update -N -i ./babel/messages.pot -d app/translations
 pybabel compile -f -d app/translations
 
--- babel_init.sh
+++ babel_init.sh
@@ -1,3 +1,3 @@
-pybabel init -i ./babel/messages.pot -d ./app/translations -l pt
+pybabel init -i ./babel/messages.pot -d ./app/translations -l $1
 pybabel compile -d ./app/translations
 
