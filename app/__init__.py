6b07a91f85b4a9ae10d76b54fb5fad9bc0bbb15a
dpgaspar <danielvazgaspar@gmail.com>
2014-05-15 23:41:50 +0100

    init correction

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,32 @@
-import os
 import logging
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.appbuilder import AppBuilder
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
-from config import basedir
 
+"""
+ Logging configuration
+"""
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 appbuilder = AppBuilder(app, db)
 
-"""
- Logging configuration
-"""
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-logging.getLogger().setLevel(logging.DEBUG)
 
 """
 Only include this for SQLLite constraints
-"""
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
-    """
-        Will force sqllite contraint foreign keys
-    """
+    # Will force sqllite contraint foreign keys
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
-    
+"""    
 
 from app import views
 
