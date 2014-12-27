feeb2c0b7124dcb357c2774c30e171e610addd91
dpgaspar <danielvazgaspar@gmail.com>
2014-12-27 14:45:21 +0000

    User AUTH consts from security manager

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
+from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Flask-WTF flag for CSRF
-CSRF_ENABLED = True
 
 # Your App secret key
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
@@ -12,32 +10,50 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 #SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
+# Flask-WTF flag for CSRF
+CSRF_ENABLED = True
+
 #------------------------------
 # GLOBALS FOR APP Builder 
 #------------------------------
-# Your App name
+# Uncomment to setup Your App name
 #APP_NAME = "My App Name"
-# Setup an App icon
+
+# Uncomment to setup Setup an App icon
 #APP_ICON = "static/img/logo.jpg"
 
 #----------------------------------------------------
 # AUTHENTICATION CONFIG
 #----------------------------------------------------
 # The authentication type
-# 0 : Is for OpenID
-# 1 : Is for database
-# 2 : Is for LDAP
-AUTH_TYPE = 1
-# Full admin role name
-AUTH_ROLE_ADMIN = 'Admin'
-# Public role name, no authentication needed
-AUTH_ROLE_PUBLIC = 'Public'
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password()
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+AUTH_TYPE = AUTH_DB
+
+# Uncomment to setup Full admin role name
+#AUTH_ROLE_ADMIN = 'Admin'
+
+# Uncomment to setup Public role name, no authentication needed
+#AUTH_ROLE_PUBLIC = 'Public'
+
 # Will allow user self registration
 #AUTH_USER_REGISTRATION = True
+
 # The default user self registration role
 #AUTH_USER_REGISTRATION_ROLE = "Public"
-# When using LDAP Auth setup the ldap server
+
+# When using LDAP Auth, setup the ldap server
 #AUTH_LDAP_SERVER = "ldap://ldapserver.new"
+
+# Uncomment to setup OpenID providers example for OpenID authentication
+#OPENID_PROVIDERS = [
+#    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 #---------------------------------------------------
 # Babel config for translations
 #---------------------------------------------------
@@ -60,22 +76,17 @@ LANGUAGES = {
 #---------------------------------------------------
 # The file upload folder, when using models with files
 UPLOAD_FOLDER = basedir + '/app/static/uploads/'
+
 # The image upload folder, when using models with images
 IMG_UPLOAD_FOLDER = basedir + '/app/static/uploads/'
+
 # The image upload url, when using models with images
 IMG_UPLOAD_URL = '/static/uploads/'
 # Setup image size default is (300, 200, True)
 #IMG_SIZE = (300, 200, True)
-# OpenID providers example for OpenID authentication
-OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
 # Theme configuration
-APP_THEME = ""                  # default
+#APP_THEME = ""                  # default
 #APP_THEME = "cerulean.css"
 #APP_THEME = "amelia.css"
 #APP_THEME = "cosmo.css"
