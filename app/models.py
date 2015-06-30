04b4ce2645853e4a6877bdebf8771d89a2a4f5f2
dpgaspar <danielvazgaspar@gmail.com>
2015-06-30 22:51:44 +0100

    #186 sqla skeleton app was still importing SQLAModel

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask.ext.appbuilder import Model
 from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
-
+from sqlalchemy import Column, Integer, String, ForeignKey 
+from sqlalchemy.orm import relationship
 """
 
 You can use the extra Flask-AppBuilder fields and Mixin's
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask.ext.appbuilder.models.datamodel import SQLAModel
+from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
 
