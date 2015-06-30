f3d937640fd3febefaa7d5ceb12824fae0607765
dpgaspar <danielvazgaspar@gmail.com>
2015-06-30 22:57:53 +0100

    views.py e example

--- app/views.py
+++ app/views.py
@@ -2,6 +2,20 @@ from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
 
+"""
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
+"""
+
 db.create_all()
 
 
