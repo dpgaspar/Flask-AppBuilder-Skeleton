ff97488e0fad7784623d8285e7195399d02a432e
dpgaspar <danielvazgaspar@gmail.com>
2015-09-17 15:34:12 +0100

    added polish translations

--- config.py
+++ config.py
@@ -68,7 +68,8 @@ LANGUAGES = {
     'es': {'flag':'es', 'name':'Spanish'},
     'de': {'flag':'de', 'name':'German'},
     'zh': {'flag':'cn', 'name':'Chinese'},
-    'ru': {'flag':'ru', 'name':'Russian'}
+    'ru': {'flag':'ru', 'name':'Russian'},
+    'pl': {'flag':'pl', 'name':'Polish'}
 }
 #---------------------------------------------------
 # Image and file configuration
