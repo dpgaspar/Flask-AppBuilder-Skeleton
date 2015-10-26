ab1acf7ad8c2b09ac690fb4ffb17759ebdfc7cfe
dpgaspar <danielvazgaspar@gmail.com>
2015-10-26 13:11:40 +0000

    included app wide 404 error handler

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
 from flask.ext.appbuilder import ModelView
 from app import appbuilder, db
@@ -15,6 +16,13 @@ from app import appbuilder, db
 
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
+
+"""
+    Application wide 404 error handler
+"""
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
 
 db.create_all()
 
